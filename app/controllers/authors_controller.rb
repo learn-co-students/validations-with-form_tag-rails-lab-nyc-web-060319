e728bbe936c32e7874ae96954b1997bec636e816
Tori Sigler <mssigler@gmail.com>
2019-07-03 13:22:53 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,14 +8,38 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      #@author.errors.full_messages
+      flash[:message]=@author.errors.full_messages
+      puts flash[:message]
+      render :new
+    end
   end
+
+  def edit 
+    @author = Author.find(params[:id])
+  end
+  
+
+  def update 
+    if @author.valid?
+      @author = Author.find(params[:id])    
+      @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      @author.errors.full_messages
+      render :edit
+    end
+  end
+
 
   private
 
   def author_params
-    params.permit(:name, :email, :phone_number)
+    params.require(:author).permit(:name, :email, :phone_number)
   end
 end
